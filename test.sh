ce5a1e8e79ef990484edaacdc7754776ac76f0e8
HSB-Labs-DWCTS <113493356+HSB-Labs-DWCTS@users.noreply.github.com>
2022-09-20 13:52:13 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,9 +5,8 @@ set -e
 
 do_install() {
   sudo apt update && sudo apt upgrade -y
-  sudo apt install python3 python3-dev git curl
+  sudo apt install -y python3 python3-dev git curl unzip
   curl -L https://tljh.jupyter.org/bootstrap.py | sudo -E python3 - --admin admin
-  sudo apt install unzip
   sudo tljh-config set user_environment.default_app jupyterlab
   sudo tljh-config reload hub
   sudo systemctl status jupyterhub
