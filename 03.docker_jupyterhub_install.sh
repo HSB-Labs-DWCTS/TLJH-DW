c13c633109cf5393c391eef3ee3ae2d4bfa62194
HSB-Labs-DWCTS <113493356+HSB-Labs-DWCTS@users.noreply.github.com>
2022-09-20 15:41:27 +0900

    Update 03.docker_jupyterhub_install.sh

--- 03.docker_jupyterhub_install.sh
+++ 03.docker_jupyterhub_install.sh
@@ -24,6 +24,8 @@ do_install() {
   docker exec -it tljh-dev sh -c "python3 /srv/src/bootstrap/bootstrap.py --admin admin"
   docker exec -it tljh-dev sh -c "tljh-config set user_environment.default_app jupyterlab"
   docker exec -it tljh-dev sh -c "tljh-config reload hub"
+  docker exec -it tljh-dev sh -c "export PATH=$PATH:/opt/tljh/user/bin"
+  docker exec -it tljh-dev sh -c "conda install -c conda-forge jupyterlab-language-pack-ko-KR -y"
   
   docker exec -it tljh-dev /bin/bash
     
