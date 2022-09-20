5ceb7ae40acc1133423fdaf8158e0b219fc2484b
HSB-Labs-DWCTS <113493356+HSB-Labs-DWCTS@users.noreply.github.com>
2022-09-20 13:56:45 +0900

    Add files via upload

---
+++ jupyterhub_install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+# JupyterHub for Linux installation script (bare metal)
+#
+
+do_install() {
+  sudo apt update && sudo apt upgrade -y
+  sudo apt install -y python3 python3-dev git curl unzip
+  curl -L https://tljh.jupyter.org/bootstrap.py | sudo -E python3 - --admin admin
+  sudo tljh-config set user_environment.default_app jupyterlab
+  sudo tljh-config reload hub
+  export PATH=$PATH:/opt/tljh/user/bin
+}
+
+#
+do_install
