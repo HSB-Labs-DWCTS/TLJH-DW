c2009ebc2b9344683ac56925aeaad9d5252e478c
HSB-Labs-DWCTS <113493356+HSB-Labs-DWCTS@users.noreply.github.com>
2022-09-21 14:31:58 +0900

    Update 01.jupyterhub_install.sh

--- 01.jupyterhub_install.sh
+++ 01.jupyterhub_install.sh
@@ -12,10 +12,11 @@ do_install() {
   curl -L https://tljh.jupyter.org/bootstrap.py | sudo -E python3 - --admin admin
   sudo tljh-config set user_environment.default_app jupyterlab
   sudo tljh-config reload hub
-  # export PATH=$PATH:/opt/tljh/user/bin
+  
+  echo 'export PATH=$PATH:/opt/tljh/user/bin' >> ~/.bashrc
   
   sudo -E /opt/tljh/user/bin/conda install -c conda-forge jupyterlab-language-pack-ko-KR -y
-  sudo sed -i 's/en_US/ko_KR/g' /home/jupyter-admin/.jupyter/lab/user-settings/@jupyterlab/translation-extension/plugin.jupyterlab-settings
+  #sudo sed -i 's/en_US/ko_KR/g' /home/jupyter-admin/.jupyter/lab/user-settings/@jupyterlab/translation-extension/plugin.jupyterlab-settings
   
   sudo -E /opt/tljh/user/bin/conda install -c conda-forge jupyterlab jupyterlab-git -y
   sudo -E /opt/tljh/user/bin/conda install -c conda-forge jupyterlab_execute_time -y
