14bd2097f0381a0e6d1bf7bb49fd4c7cae279e9c
HSB-Labs-DWCTS <113493356+HSB-Labs-DWCTS@users.noreply.github.com>
2022-10-12 17:46:28 +0900

    Update bootstrap.py

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -325,7 +325,7 @@ def _resolve_git_version(version):
 
 
 def installExtension():
-    subprocess.run(["sudo", "tljh-config", "set", "user_environment.default_app", "jupyterlab"], check=True)
+    subprocess.run(["sudo", "tljh-config", "set", "user_environment.default_app", "jupyterlab"])
 
 def main():
     """
