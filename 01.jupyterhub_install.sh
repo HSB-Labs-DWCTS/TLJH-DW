a90e019cd55bc42035c8b003558b37a56e181182
HSB-Labs-DWCTS <113493356+HSB-Labs-DWCTS@users.noreply.github.com>
2022-10-12 09:50:38 +0900

    Update 01.jupyterhub_install.sh

--- 01.jupyterhub_install.sh
+++ 01.jupyterhub_install.sh
@@ -26,6 +26,7 @@ do_install() {
   # jupyterlab-git, jupyterlab_nvdashboard, jupyterlab_execute_time, jupyterlab-drawio
   sudo -E /opt/tljh/user/bin/conda install -c conda-forge jupyterlab-git jupyterlab_execute_time -y
   sudo -E /opt/tljh/user/bin/pip install jupyterlab_nvdashboard
+  sudo -E /opt/tljh/user/bin/pip install lckr-jupyterlab-variableinspector
   
   # Config
   # echo 'export PATH=$PATH:/opt/tljh/user/bin' >> ~/.bashrc
