3b39d8ccd3100297e119e478a61eb47891ac22a8
HSB-Labs-DWCTS <113493356+HSB-Labs-DWCTS@users.noreply.github.com>
2022-10-14 10:08:45 +0900

    Update 01.jupyterhub_install.sh

--- 01.jupyterhub_install.sh
+++ 01.jupyterhub_install.sh
@@ -6,39 +6,31 @@ set -e
 # Install JupyterHub (bare metal)
 #
 
-do_install() {
-  sudo apt update && sudo apt upgrade -y
-  sudo apt install python3 python3-dev git curl -y
+doInstall() {
+  sudo apt update
+  # sudo apt upgrade --yes
+  sudo apt install python3 python3-dev git curl --yes
   curl -sL https://raw.githubusercontent.com/HSB-Labs-DWCTS/the-littlest-jupyterhub-dwcts/main/bootstrap.py | sudo -E python3 - --admin admin
   
+  # Settings
   # Change default User Interface for users
-  sudo tljh-config set user_environment.default_app jupyterlab  
+  sudo tljh-config set user_environment.default_app jupyterlab
   
   # Enable PAM Authenticator
   # sudo tljh-config set auth.type jupyterhub.auth.PAMAuthenticator
   sudo tljh-config reload
   
   # JupyterLab ko-KR Language Pack
-  sudo -E /opt/tljh/user/bin/conda install -c conda-forge jupyterlab-language-pack-ko-KR -y
+  sudo -E /opt/tljh/user/bin/conda install -c conda-forge --yesjupyterlab-language-pack-ko-KR --yes
   # sudo sed -i 's/en_US/ko_KR/g' /home/jupyter-admin/.jupyter/lab/user-settings/@jupyterlab/translation-extension/plugin.jupyterlab-settings
   
   # Extension
   # jupyterlab-git, jupyterlab-github, jupyterlab_nvdashboard, jupyterlab_execute_time, lckr-jupyterlab-variableinspector, jupyterlab-drawio,ipysheet
   # conda-forge
-  sudo -E /opt/tljh/user/bin/conda install -c conda-forge jupyterlab-git -y
-  sudo -E /opt/tljh/user/bin/conda install -c conda-forge jupyterlab_execute_time -y
-  sudo -E /opt/tljh/user/bin/conda install -c conda-forge jupyterlab-drawio -y
-  sudo -E /opt/tljh/user/bin/conda install -c conda-forge ipysheet -y
-  sudo -E /opt/tljh/user/bin/conda install -c conda-forge theme-darcula -y
-  
-  # pip
-  sudo -E /opt/tljh/user/bin/pip install jupyterlab_nvdashboard
-  sudo -E /opt/tljh/user/bin/pip install lckr-jupyterlab-variableinspector
-  sudo -E /opt/tljh/user/bin/pip install jupyterlab-github
-  sudo -E /opt/tljh/user/bin/pip install jupyterlab_materialdarker
-  
+  sudo -E /opt/tljh/user/bin/conda install -c conda-forge --yes jupyterlab-git jupyterlab-github jupyterlab_execute_time jupyterlab-nvdashboard jupyterlab-variableinspector jupyterlab-drawio
+    
   # Extension list
-  sudo /opt/tljh/user/bin/jupyter server extension list
+  # sudo /opt/tljh/user/bin/jupyter server extension list
   
   # Config
   # echo 'export PATH=$PATH:/opt/tljh/user/bin' >> ~/.bashrc
@@ -48,4 +40,4 @@ do_install() {
 #
 # 
 #
-do_install
+doInstall
