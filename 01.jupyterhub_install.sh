ced13d876537b5569848ac595b3c5b0ac9e6f29c
HSB-Labs-DWCTS <113493356+HSB-Labs-DWCTS@users.noreply.github.com>
2022-10-18 13:35:36 +0900

    Update 01.jupyterhub_install.sh

--- 01.jupyterhub_install.sh
+++ 01.jupyterhub_install.sh
@@ -9,7 +9,9 @@ set -e
 doInstall() {
   sudo apt update
   # sudo apt upgrade --yes
-  sudo apt install python3 python3-dev git curl --yes
+  sudo apt install -y build-essential
+  curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - && sudo apt install -y nodejs
+  sudo apt install -y python3 python3-dev git curl
   curl -sL https://raw.githubusercontent.com/HSB-Labs-DWCTS/the-littlest-jupyterhub-dwcts/main/bootstrap.py | sudo -E python3 - --admin admin
   
   # Settings
@@ -21,12 +23,12 @@ doInstall() {
   sudo tljh-config reload
   
   # JupyterLab ko-KR Language Pack
-  sudo -E /opt/tljh/user/bin/conda install -c conda-forge --yes jupyterlab-language-pack-ko-KR
+  sudo -E /opt/tljh/user/bin/conda install -c conda-forge -y jupyterlab-language-pack-ko-KR
   # sudo sed -i 's/en_US/ko_KR/g' /home/jupyter-admin/.jupyter/lab/user-settings/@jupyterlab/translation-extension/plugin.jupyterlab-settings
   
   # Extension
   # conda-forge
-  sudo -E /opt/tljh/user/bin/conda install -c conda-forge --yes jupyterlab-git jupyterlab-github jupyterlab_execute_time jupyterlab-variableinspector jupyterlab-drawio ipywidgets
+  sudo -E /opt/tljh/user/bin/conda install -c conda-forge -y jupyterlab-git jupyterlab-github jupyterlab_execute_time jupyterlab-variableinspector jupyterlab-drawio ipywidgets
   sudo -E /opt/tljh/user/bin/pip install jupyterlab-nvdashboard nbconvert
     
   # Extension list
